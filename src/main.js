0f58bfc66425c03b68ae3b1b85dae774a1d6809c
Kaleem Qasim <kaleemullahlqp@gmail.com>
2023-02-11 01:00:13 +0500

    fa webfonts

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './assets/css/bootstrap.min.css'
 import './assets/css/funchy.css'
 import './assets/css/style.css'
-// import './assets/css/fontawesome.min.css'
+import './assets/css/fontawesome.min.css'
 
 
 createApp(App).mount('#app')
